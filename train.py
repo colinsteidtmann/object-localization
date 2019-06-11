83c8aec4c80ba398329a83ad255feb9e2153c717
Colin Steidtmann <colinsteidtmann@yahoo.com>
2019-06-10 17:03:36 -0700

    updaing train.py

--- train.py
+++ train.py
@@ -98,9 +98,11 @@ def create_model():
     box_conv_layer3 = tf.keras.layers.Flatten(name='box_output')(box_conv_layer3)
     
     """classConvnet branch"""
-    class_conv_layer1 = tf.keras.layers.Conv2D(filters=4096, kernel_size=[4, 4], strides=[1, 1], padding="valid",data_format="channels_last", activation="relu")(norm_pool_layer5)
-    class_conv_layer2 = tf.keras.layers.Conv2D(filters=1024, kernel_size=[1, 1], strides=[1, 1], padding="same",data_format="channels_last", activation="relu")(class_conv_layer1)
-    class_conv_layer3 = tf.keras.layers.Conv2D(filters=NUM_CLASSES, kernel_size=[1, 1], strides=[1, 1], padding="same", data_format="channels_last", activation="softmax")(class_conv_layer2)
+    class_conv_layer1 = tf.keras.layers.Conv2D(filters=4096, kernel_size=[4, 4], strides=[1, 1], padding="valid", data_format="channels_last", activation="relu")(norm_pool_layer5)
+    class_conv_layer1_dropout = tf.keras.layers.Dropout(rate=0.4)(class_conv_layer1)
+    class_conv_layer2 = tf.keras.layers.Conv2D(filters=1024, kernel_size=[1, 1], strides=[1, 1], padding="same", data_format="channels_last", activation="relu")(class_conv_layer1_dropout)
+    class_conv_layer2_dropout = tf.keras.layers.Dropout(rate=0.4)(class_conv_layer2)
+    class_conv_layer3 = tf.keras.layers.Conv2D(filters=NUM_CLASSES, kernel_size=[1, 1], strides=[1, 1], padding="same", data_format="channels_last", activation="softmax")(class_conv_layer2_dropout)
     class_conv_layer3 = tf.keras.layers.Flatten(name='class_output')(class_conv_layer3)
 
     model_optimizer = tf.keras.optimizers.Adam(lr=0.001)
@@ -108,7 +110,7 @@ def create_model():
     model.compile(optimizer=model_optimizer,
                 loss=[tf.keras.losses.MeanAbsoluteError(), tf.keras.losses.CategoricalCrossentropy()],
                 loss_weights={'box_output': 10.,
-                        'class_output': 0.5},
+                        'class_output': 0.3},
                 metrics={'box_output': 'mean_absolute_error',
                         'class_output': 'categorical_accuracy'})
 
